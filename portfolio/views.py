75ed203b32126d564f929de8f3921a1762fad7f1
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-08-22 15:30:59 +0200

    Add

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import User, Project
 
 # Create your views here.
+
+def home(request):
+    user=User.objects.get(name='Ahmad')
+    context={'user':user}
+    return render(request, 'home.html', context)
+
+
+def projects(request):
+    projects=Project.objects.all()
+    context={'projects':projects}
+    return render(request, 'projects.html',context)
