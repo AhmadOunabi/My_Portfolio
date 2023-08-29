e6a9f49e901b338359fd34090045b2cd39bb1ffc
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-08-29 21:10:23 +0200

    Add

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display= ['title','content','tag','created_date']
+
+
+
+admin.site.register(Post,PostAdmin)
